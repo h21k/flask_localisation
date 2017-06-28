6fb145a84d63e1f5bc16f8be3776d61c8a55e9ba
frank soboczenski <frank.soboczenski@gmail.com>
2017-06-28 19:22:35 +0100

    initial commit

---
+++ routes.py
@@ -0,0 +1,15 @@
+# import Flask class and render function
+from flask import Flask, render_template
+
+# create a new usable instance of the flask class and save it into variable app
+app = Flask(__name__)
+
+# map URL to flask function
+@app.route('/')
+    # here / is mapped to the index function
+def index():
+        # python function uses render_template function to return index.html
+    return render_template("index.html")
+    # turn on debug=true to see error messages
+if __name__ == "__main__":
+    app.run(debug=True)
