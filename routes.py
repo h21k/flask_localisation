66783ba9c911ee032812037826348852fe73403c
frank soboczenski <frank.soboczenski@gmail.com>
2017-06-28 20:08:19 +0100

    fourth commit

--- routes.py
+++ routes.py
@@ -8,8 +8,16 @@ app = Flask(__name__)
 @app.route('/')
     # here / is mapped to the index function
 def index():
-        # python function uses render_template function to return index.html
+    # python function uses render_template function to return index.html
     return render_template("index.html")
+
+@app.route('/about')
+    # map url about to python function about
+def about():
+    # python function uses render_template function to return about.html
+    return render_template("about.html")
+
+
     # turn on debug=true to see error messages
 if __name__ == "__main__":
     app.run(debug=True)
